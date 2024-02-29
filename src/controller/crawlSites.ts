b7efafa5809bbe39f774c0a4bef8d3cfd2f5acdd
timfemey <isholaobafemi@gmail.com>
2024-02-29 14:35:56 +0100

    push links found to array for data

--- src/controller/crawlSites.ts
+++ src/controller/crawlSites.ts
@@ -1,12 +1,12 @@
-import { extractContent } from "../helpers/extractContent";
+import { extractContent } from "../helpers/extractContent.js";
 
 export default function crawlSites(sites: string[]) {
-    const dataRes: string[][] = []
+    const dataRes: string[] = []
     if (Array.isArray(sites)) {
         for (let index = 0; index < sites.length; index++) {
             const site = sites[index];
             extractContent(site).then((linksArr) => {
-                // dataRes.push(linksArr)
+                dataRes.push(...linksArr)
             })
 
 
